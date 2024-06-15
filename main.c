e310388f5c15413611fccb445dc15462c3a79de7
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-06-15 15:39:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #define BVM_IMPLEMENTATION
 #include "bvm.h"
-
+#define BVMASM_IMPLEMENTATION
+#include "bvmasm.h"
 int main(){
+	
+	
 	Bvm bvm = initBVM();
 	//executeInstruction(&bvm);
 	//stackPush()
-	textToProgram("program.bvm", bvm.instruction);
+	bvm.numOfInstructions =  textToProgram("fibonaci.bvm", bvm.instruction);
+	LOG("Num of instruction, %d\n", bvm.numOfInstructions);
+	programToBin("program.bin", bvm.instruction, bvm.numOfInstructions);
+	//binToProgram("program.bin", bvm.instruction);
 	//LOG("%d\n\n", bvm.instruction[0].type);
+	
 	loop(&bvm);
 	return 0;
 }
+
+
+
+
+
+
 
 
 
