fab54b1f592f17260d67c661fc175131c4cf94f4
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-01-31 09:30:25 +0100

    Update bvm.h

--- bvm.h
+++ bvm.h
@@ -1,20 +1,28 @@
+
 #ifndef BVM_H
 #define BVM_H
 #include<stdint.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
-
+#include<unistd.h>
+#include<signal.h>
+#include<stddef.h>
 #define LOG(...)         fprintf(stdout, __VA_ARGS__)
 #define LOGSTACK()        LOG("Value %d\n", (i16)vm.stack[SP].asI64);
 #define ERROR_BREAK(...) {fprintf(stdout, __VA_ARGS__); exit(-1);}
-#define PAUSE()        	 system("pause")
+#define PAUSE()        	 {char ch; scanf("%c", &ch);}
 
 #define DEBUG 1
 #define TRUE  1
 #define FALSE 0
 
-enum {i, f, u, ch};
+#define SYSCALLS
+#define SYSTEM
+
+
+//#define LOG_STACK
+enum {i = 0, f, u, ch};
 
 //
 
@@ -39,58 +47,127 @@ typedef union {
 	} Word;
 
 typedef enum {
-	
+	POP,
 	PUSH,
 	PUSHF,
 	PUSHIP,
-	POP,
+	PUSHSP,
 	PRINT,
+	PRINTSTACK,
+	PRINTSTRING,
 	ADD,
 	MUL,
 	DEC,
 	DIV,
+	MOD,
+	NOT,
+	SHR,
+	SHL,
+	OR,
+	AND,
+	BNOT,
 	DUP,
+	OVER,
+	ROT,
+	SWAP_NO,
 	IF,
 	JMP,
 	JMPT,
+	JMPF,
 	SETSP,
+	SETSPSTACK,
+	RESTORE,
 	COPY,
+	COPYSTACK,
 	SWAP,
+	MEM,
+	MEMSTACK,
 	NOP,
 	HALT,
 	INC,
+#ifdef SYSCALLS
+	WRITE,
+	CLOSE,
+	DUPF,
+	DUP2,
+	EXIT,
+	TRUNCATE,
+	ISATTY,
+	READ,
+	SWAB,
+	SLEEP,
+#ifdef SYSTEM
+	SYSTEMS,
+#endif
+#endif
 	NEWLINE,
 	LABEL,
+	FFI,
 	END,
-	
+
 
 	} InstructionType;
 
-const char* instructionNames[] = {
+static const char* instructionNames[] = {
 
+	"POP",
 	"PUSH",
 	"PUSHF",
 	"PUSHIP",
-	"POP",
+	"PUSHSP",
 	"PRINT",
+	"PRINTSTACK",
+	"PRINTSTRING",
 	"ADD",
 	"MUL",
 	"DEC",
 	"DIV",
+	"MOD",
+	"NOT",
+	"SHR",
+	"SHL",
+	"OR",
+	"AND",
+	"BNOT",
 	"DUP",
+	"OVER",
+	"ROT",
+	"SWAP_NO",
 	"IF",
 	"JMP",
 	"JMPT",
+	"JMPF",
 	"SETSP",
+	"SETSPSTACK",
+	"RESTORE",
 	"COPY",
+	"COPYSTACK",
 	"SWAP",
+	"MEM",
+	"MEMSTACK",
 	"NOP",
 	"HALT",
 	"INC",
+#ifdef SYSCALLS
+	"WRITE",
+	"CLOSE",
+	"DUPF",
+	"DUP2",
+	"EXIT",
+	"TRUNCATE",
+	"ISATTY",
+	"READ",
+	"SWAB",
+	"SLEEP",
+#ifdef SYSTEM
+	"SYSTEM",
+#endif
+#endif
 	"\n",
 	"LABEL",
+	"FFI",
 	"END",
-};
+	};
 
 //SIZE OF THE INSTRUCTION IS 8BYTE + 1BYTE
 typedef struct {
@@ -99,11 +176,11 @@ typedef struct {
 	} Instruction;
 
 
-#define STACK_CAPACITIY     102400
-#define MAX_SIZE_OF_PROGRAM 10024
+#define STACK_CAPACITIY     200024
+#define MAX_SIZE_OF_PROGRAM 20024
 #define MAX_LINE 100
 typedef struct {
-	Word stack[STACK_CAPACITIY];
+	Word *stack;
 	i64  SP;
 	} Stack;
 
@@ -111,13 +188,14 @@ static inline void stackPush(Stack *stack,i64 value);
 static inline void stackPushF64(Stack *stack,f64 value);
 static inline void stackPushWord(Stack *stack, Word value);
 static inline Word stackPop(Stack *stack);
-
+static inline void initStack(Stack *stack);
 typedef struct {
 	u8 isRuning;
 	Stack stack;
 	Instruction instruction[MAX_SIZE_OF_PROGRAM];
 	u64 numOfInstructions;
 	u64 IP;
+	i64 jumpReg;
 	} Bvm;
 
 static inline Bvm  initBVM(void);
@@ -126,14 +204,14 @@ static inline void loop(Bvm *bvm);
 
 
 static inline u64 textToProgram(const char* name, Instruction *instrucion);
-static inline void dissasembler(Bvm *bvm);
+//static inline void dissasembler(Bvm *bvm);
 static inline void programToBin(const char* name, Instruction *instruction, u64 numOfInstruction);
 static inline void binToProgram(const char* name, Instruction *instruction);
 
 
-#define BVM_IMPLEMENTATION
+//#define BVM_IMPLEMENTATION
 
-
+#ifdef BVM_IMPLEMENTATION
 //STACK OPERATIONS
 //LETS DEFULT BE u64
 static inline void stackPush(Stack *stack,i64 value) {
@@ -141,7 +219,9 @@ static inline void stackPush(Stack *stack,i64 value) {
 		ERROR_BREAK("STACK OVERFLOW!!!\n");
 		}
 	stack->stack[stack->SP++]._asI64 =  value;
-	//LOG("STACK PUSH %d\n", stack->stack[stack->SP - 1]._asI64);
+#ifdef LOG_STACK
+	LOG("STACK PUSH %d\n", stack->stack[stack->SP - 1]._asI64);
+#endif
 	}
 
 
@@ -151,54 +231,69 @@ static inline void stackPushF64(Stack *stack,f64 value) {
 		ERROR_BREAK("STACK OVERFLOW!!!\n");
 		}
 	stack->stack[stack->SP++]._asF64 =  value;
-	//LOG("STACK PUSH %d\n", stack->stack[stack->SP - 1]._asI64);
+#ifdef LOG_STACK
+	LOG("STACK PUSH %f\n", stack->stack[stack->SP - 1]._asF64);
+#endif
 	}
-static inline void stackPushWord(Stack *stack, Word value){
-	if(stack->SP > STACK_CAPACITIY){
+static inline void stackPushWord(Stack *stack, Word value) {
+	if(stack->SP > STACK_CAPACITIY) {
 		ERROR_BREAK("STACK OVERFLOW!!!\n");
+		}
+#ifdef LOG_STACK
+	LOG("STACK PUSH %d\n", stack->stack[stack->SP - 1]._asI64);
+#endif
+	stack->stack[stack->SP++] = value;
 	}
-	stack->stack[stack->SP++] = value; 
-}
 
 
 static inline Word stackPop(Stack *stack) {
-	if(stack->SP < 0) {
+	if(stack->SP <= 0) {
 		ERROR_BREAK("STACK UNDERFLOW!!!\n");
 
 		}
 	stack->SP--;
-	//LOG("STACK POP %d\n", stack->stack[stack->SP]._asI64);
+#ifdef LOG_STACK
+	LOG("STACK POP %d\n", stack->stack[stack->SP]._asI64);
+#endif
 	return stack->stack[stack->SP];
+	}
+
+static inline void initStack(Stack *stack) {
+	stack->SP = 0;
+	stack->stack = malloc(sizeof(Word) * STACK_CAPACITIY);
 	}
 
 
 
 static inline Bvm initBVM(void) {
 	Bvm bvm;
-
 	//PROBOBLY DYNAMIC ALLOCATION OF STACK AND INSTRUCTION WHEN FAIL
 	//ALL mem FUNCTIONS
-
 	memset(&bvm, 0, sizeof(bvm));
 	bvm.isRuning = TRUE;
+	initStack(&bvm.stack);
 	LOG("Init BVM\n");
 	LOG("\n SP = %lu\n", bvm.stack.SP);
 	LOG("\n IP = %lu\n", bvm.IP);
-
-
+	bvm.numOfInstructions = 0;
 	return bvm;
-
-
 	}
 
 static inline void executeInstruction(Bvm *bvm) {
 
-	static Word a, b, c;
-
-
+	Word a, b, c;
+#ifdef LOG_STACK
+	printf("\n- %s\n", instructionNames[bvm->instruction[bvm->IP].type]);
+#endif
 	//system("pause");
 	switch(bvm->instruction[bvm->IP].type) {
-	
+
+		case POP: {
+				a = stackPop(&bvm->stack);
+				bvm->IP++;
+				break;
+				}
+
 		case PUSH: {
 				stackPush(&bvm->stack, bvm->instruction[bvm->IP].operand._asI64);
 				bvm->IP++;
@@ -217,12 +312,12 @@ static inline void executeInstruction(Bvm *bvm) {
 				break;
 				}
 
-
-		case POP: {
-				a = 	stackPop(&bvm->stack);
+		case PUSHSP: {
+				stackPush(&bvm->stack, (i64)bvm->stack.SP);
 				bvm->IP++;
 				break;
 				}
+
 		case ADD: {
 				a = stackPop(&bvm->stack);
 				b = stackPop(&bvm->stack);
@@ -233,35 +328,59 @@ static inline void executeInstruction(Bvm *bvm) {
 					stackPush(&bvm->stack, (a._asI64 + b._asI64));
 					}
 				else if(c._asU64 == f) {
-					stackPushF64(&bvm->stack, (a._asF64 + b._asF64));
+					stackPushF64(&bvm->stack, (f64)(a._asF64 + b._asF64));
 					}
 				else {}; //PTR MAYBE
 				bvm->IP++;
 				break;
-
 				}
 		case PRINT: {
 				c = bvm->instruction[bvm->IP].operand;
 				if(c._asU64 == u)
-					LOG("PRINT %u\n\n", bvm->stack.stack[bvm->stack.SP - 1]._asU64);
+					LOG("%u\n\n", bvm->stack.stack[bvm->stack.SP - 1]._asU64);
 				else if(c._asU64 == i) {
-					LOG("PRINT %d\n\n", bvm->stack.stack[bvm->stack.SP - 1]._asI64);
+					LOG("%d\n\n", bvm->stack.stack[bvm->stack.SP - 1]._asI64);
 					}
 				else if(c._asU64 == f) {
-					LOG("PRINT %f\n\n", bvm->stack.stack[bvm->stack.SP - 1]._asF64);
+					LOG("%f\n\n", (float)bvm->stack.stack[bvm->stack.SP - 1]._asF64);
 					}
-					else if(c._asU64 == ch) {
-					LOG("%c", (char)bvm->stack.stack[bvm->stack.SP - 1]._asU64);
+				else if(c._asU64 == ch) {
+					a = stackPop(&bvm->stack);
+					LOG("%c", (char)a._asU64);
+
+					//write(2, &bvm->stack.stack[bvm->stack.SP - 1]._asU64, 1);
 					}
 
-
+				//system("pause");
 				bvm->IP++;
 				break;
 				}
 
+		case PRINTSTACK: {
+				LOG("\n-----------------------------------\n");
+				LOG("Stack : \n");
+				for(int i = bvm->stack.SP-1; i >= 0 ; i--) {
+					LOG("%u\n", bvm->stack.stack[i]);
+					}
+				LOG("\n-----------------------------------\n");
+				bvm->IP++;
+				break;
+				}
+
+		case PRINTSTRING: {
+				int counter = 0;
+				for(counter = bvm->stack.SP - 1;  bvm->stack.stack[counter]._asU64 != 0 && counter > 0; counter--) {
+					//printf("counter %d\n", counter);
+					}
+				//
+				for(int i = counter; i < bvm->stack.SP; i++) {
+					LOG("%c", bvm->stack.stack[i]);
+					}
+				bvm->IP++;
+				break;
+				}
 
 		case MUL: {
-
 				a = stackPop(&bvm->stack);
 				b = stackPop(&bvm->stack);
 				c = bvm->instruction[bvm->IP].operand;
@@ -315,11 +434,95 @@ static inline void executeInstruction(Bvm *bvm) {
 				bvm->IP++;
 				break;
 				}
+		case MOD: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				if(b._asI64 == 0) {
+					ERROR_BREAK("ERROR MOD BY 0 !!!\n");
+					}
+				stackPush(&bvm->stack, (i64)(a._asU64 % b._asU64));
+				break;
+				}
+		case NOT: {
+				a = stackPop(&bvm->stack);
+				a._asI64 = !a._asI64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+		case SHR: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				a._asU64 = a._asU64 >> b._asU64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+		case SHL: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				a._asU64 = a._asU64 << b._asU64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+		case OR: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				a._asU64 = a._asU64 | b._asU64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+		case AND: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				a._asU64 = a._asU64 & b._asU64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+		case BNOT: {
+				a = stackPop(&bvm->stack);
+				a._asU64 = ~a._asU64;
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
 
 		case DUP: {
 				a = stackPop(&bvm->stack);
-				stackPush(&bvm->stack, a._asI64);
-				stackPush(&bvm->stack, a._asI64);
+				stackPushWord(&bvm->stack, a);
+				stackPushWord(&bvm->stack, a);
+				bvm->IP++;
+				break;
+				}
+
+		case OVER: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				stackPushWord(&bvm->stack, b);
+				stackPushWord(&bvm->stack, a);
+				stackPushWord(&bvm->stack, b);
+				bvm->IP++;
+				break;
+				}
+		case ROT: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				c = stackPop(&bvm->stack);
+				stackPushWord(&bvm->stack, a);
+				stackPushWord(&bvm->stack, c);
+				stackPushWord(&bvm->stack, b);
+				bvm->IP++;
+				break;
+				}
+
+		case SWAP_NO: {
+				a = stackPop(&bvm->stack);
+				b = stackPop(&bvm->stack);
+				stackPushWord(&bvm->stack, a);
+				stackPushWord(&bvm->stack, b);
 				bvm->IP++;
 				break;
 				}
@@ -369,18 +572,35 @@ static inline void executeInstruction(Bvm *bvm) {
 				break;
 				}
 
-		case JMP: {
-				a = bvm->instruction[bvm->IP].operand;
 
-				bvm->IP = a._asU64;
+		case JMP: {
+				//a = stackPop(&bvm->stack);
+				b = bvm->instruction[bvm->IP].operand;
+				bvm->IP = b._asU64;
 				break;
 				}
 
 		case JMPT: {
 				a = stackPop(&bvm->stack);
 				c =  bvm->instruction[bvm->IP].operand;
-				
+
 				if(a._asU64) {
+					bvm->IP = c._asU64;
+					bvm->jumpReg = 1;
+					//stackPush(&bvm->stack, a._asI64);
+					}
+				else {
+					//stackPush(&bvm->stack, a._asI64);
+					bvm->IP++;
+					}
+				break;
+				}
+
+		case JMPF: {
+				a = stackPop(&bvm->stack);
+				c =  bvm->instruction[bvm->IP].operand;
+
+				if(!a._asU64) {
 					bvm->IP = c._asU64;
 					//stackPush(&bvm->stack, a._asI64);
 					}
@@ -390,6 +610,7 @@ static inline void executeInstruction(Bvm *bvm) {
 					}
 				break;
 				}
+
 
 		case SETSP: {
 				a = bvm->instruction[bvm->IP].operand;
@@ -401,28 +622,79 @@ static inline void executeInstruction(Bvm *bvm) {
 				break;
 				}
 
-		case COPY: {
+		case SETSPSTACK: {
 				a = stackPop(&bvm->stack);
-				b = bvm->instruction[bvm->IP].operand;
-				stackPush(&bvm->stack, a._asI64);
-				if(b._asU64 > STACK_CAPACITIY) {
+				if(a._asU64 >= STACK_CAPACITIY) {
 					ERROR_BREAK("STACK OVERFLOW ACCES!!!\n");
 					}
-				bvm->stack.stack[b._asU64] = a;
+				bvm->stack.SP = a._asI64;
 				bvm->IP++;
 				break;
 				}
 
+
+		case RESTORE: {
+				//s[sp] -> sp, operand ->  sp
+				a = stackPop(&bvm->stack);
+				b = bvm->instruction[bvm->IP].operand;
+				if(b._asU64 > STACK_CAPACITIY) {
+					ERROR_BREAK("STACK OVERFLOW ACCES!!!\n");
+					}
+				bvm->stack.SP = b._asI64;
+				bvm->IP = a._asU64;
+				break;
+				}
+
+
+		case COPY: {
+				//a = stackPop(&bvm->stack);
+				b = bvm->instruction[bvm->IP].operand;
+				c = bvm->stack.stack[b._asU64];
+				stackPushWord(&bvm->stack, c);
+				//bvm->stack.stack[b._asU64] = a;
+				bvm->IP++;
+				break;
+
+				}
+
+		case COPYSTACK: {
+				a = stackPop(&bvm->stack); //memadress
+				//b = bvm->instruction[bvm->IP].operand;
+				c = bvm->stack.stack[a._asU64]; //value of memadress
+				stackPushWord(&bvm->stack, c);
+				bvm->IP++;
+				break;
+
+				}
 		case SWAP: {
 				a = stackPop(&bvm->stack);
 				b = bvm->instruction[bvm->IP].operand;
 				c = bvm->stack.stack[b._asU64];
-				stackPush(&bvm->stack, c._asI64);
+				stackPushWord(&bvm->stack, c);
 				bvm->stack.stack[b._asU64] = a;
 				bvm->IP++;
 				break;
 				}
 
+		case MEM: {
+				a = stackPop(&bvm->stack);
+				b = bvm->instruction[bvm->IP].operand;
+				c = bvm->stack.stack[b._asU64];
+				stackPushWord(&bvm->stack, a);
+				memcpy(&bvm->stack.stack[b._asU64], &a, sizeof(Word));
+				bvm->IP++;
+				break;
+				}
+		case MEMSTACK: {
+				a = stackPop(&bvm->stack); // memaddres
+				b = stackPop(&bvm->stack); // value to print
+				stackPushWord(&bvm->stack, b);
+				//bvm->stack.stack[a._asI64]._asI64 = 100;
+				memcpy(&bvm->stack.stack[a._asI64], &b, sizeof(Word));
+				bvm->IP++;
+				break;
+
+				}
 
 		case NOP: {
 				bvm->IP++;
@@ -443,6 +715,134 @@ static inline void executeInstruction(Bvm *bvm) {
 				break;
 				}
 
+			//SYSCALL
+#ifdef SYSCALLS
+		case WRITE: {
+				a = stackPop(&bvm->stack);//size
+				b = stackPop(&bvm->stack);//data
+				c = stackPop(&bvm->stack);//fd
+				//printf("str: %d\n", &bvm->stack.stack[b._asI64]);
+				u8 bytes[a._asI64];
+				memset(bytes, 0, sizeof(u8)*a._asI64);
+				int counter = 0;
+
+				//TBD Order of a strings when pushed on stack
+				for(int i = 1; i < a._asI64 ; i++) {
+					bytes[i] = (u8)((bvm->stack.stack[b._asI64+i])._asU64);
+					}
+				//printf("str: %s\n", bytes);
+				const i64 temp = write((int)c._asI64, bytes, (u32)a._asU64);
+				stackPush(&bvm->stack, temp);
+				bvm->IP++;
+				break;
+				}
+
+		case CLOSE: {
+				a = stackPop(&bvm->stack);//size
+				const int temp = close(a._asI64);
+				stackPush(&bvm->stack, (i64)temp);
+				bvm->IP++;
+				break;
+				}
+
+		case DUPF: {
+				a = stackPop(&bvm->stack);//size
+				const int temp = dup(a._asI64);
+				stackPush(&bvm->stack, (i64)temp);
+				bvm->IP++;
+				break;
+				}
+		case DUP2: {
+				a = stackPop(&bvm->stack);//size
+				b = stackPop(&bvm->stack);
+				const int temp = dup2(b._asI64, a._asI64);
+				stackPush(&bvm->stack, (i64)temp);
+				bvm->IP++;
+				break;
+				}
+		case EXIT: {
+				a = stackPop(&bvm->stack);
+				_exit(a._asI64);
+				bvm->IP++;
+				break;
+				}
+		case TRUNCATE: {
+				a = stackPop(&bvm->stack);//ofset
+				b = stackPop(&bvm->stack);//fd
+				const i64 temp = ftruncate(b._asI64, a._asI64);
+				stackPush(&bvm->stack, temp);
+				bvm->IP++;
+				break;
+				}
+
+		case ISATTY: {
+				a = stackPop(&bvm->stack);//ofset
+				const i64 temp = isatty(a._asI64);
+				stackPush(&bvm->stack, temp);
+				bvm->IP++;
+				break;
+				}
+		case READ: {
+				a = stackPop(&bvm->stack);//size
+				b = stackPop(&bvm->stack);//data
+				c = stackPop(&bvm->stack);//fd
+				//printf("str: %d\n", &bvm->stack.stack[b._asI64]);
+				u8 bytes[a._asI64];
+				//memset(bytes, 0, sizeof(u8)*a._asI64);
+				const i64 temp = read((int)c._asI64, bytes, (u32)a._asU64);
+				int counter = 0;
+
+				//TBD Order of a strings when pushed on stack
+				for(int i = 0; i < a._asI64 ; i++) {
+					bvm->stack.stack[b._asI64+i]._asU64 = (u64)bytes[i];
+					}
+				//printf("str: %s\n", bytes);
+
+				stackPush(&bvm->stack, temp);
+				bvm->IP++;
+				break;
+				}
+
+		case SWAB: {
+				a = stackPop(&bvm->stack);//size
+				b = stackPop(&bvm->stack);//dest
+				c = stackPop(&bvm->stack);//source
+				//printf("str: %d\n", &bvm->stack.stack[b._asI64]);
+				swab((char*)&bvm->stack.stack[b._asI64]._asU64, (char*)&bvm->stack.stack[a._asI64], a._asI64 * sizeof(Word));
+				bvm->IP++;
+				break;
+				}
+
+		case SLEEP: {
+				a = stackPop(&bvm->stack);
+				const u64 temp = sleep(a._asU64);
+				stackPush(&bvm->stack, (i64)temp);
+				bvm->IP++;
+				break;
+				}
+#ifdef SYSTEM
+		case SYSTEMS: {
+
+				int counter = 0, counter1 = 0;
+				char bytes[100] = {'\0'};
+				for(counter = bvm->stack.SP - 1;  bvm->stack.stack[counter]._asU64 != 0 && counter > 0; counter--) {
+
+					}
+				for(int i = counter; i < bvm->stack.SP; i++) {
+					bytes[counter1++] = (char)bvm->stack.stack[i]._asU64;
+					//printf("%c\n", bytes[counter1-1]);
+					if(counter1 == 99) {
+						break;
+						}
+					}
+				bytes[counter1] = '\0';
+				const int temp = system(&bytes[1]);
+				stackPush(&bvm->stack, temp);
+				bvm->IP++;
+				break;
+				}
+#endif
+#endif
 		case END: {
 				bvm->isRuning = FALSE;
 				LOG("\n\nExiting VM\n\n!!!");
@@ -498,6 +898,7 @@ static inline u64 textToProgram(const char* name, Instruction *instrucion) {
 			}
 		token[counter] = '\0';
 		LOG("Instruction %s\n", token);
+		//system("pause");
 		counter++;
 		while(instructionText[counter] != ' '  && counter < 59
 		      && instructionText[counter] != '\0' &&  counterOperand < 10) {
@@ -515,11 +916,11 @@ static inline u64 textToProgram(const char* name, Instruction *instrucion) {
 					instrucion[counterInstruction].operand._asF64 = (f64)atof(textOperand);
 					LOG("operand %f\n", instrucion[counterInstruction].operand._asF64);
 					}
-				
-				else if(inst == NEWLINE || inst == LABEL){
+
+				else if(inst == NEWLINE || inst == LABEL) {
 					//DO NOTING IF NEW LINE
 					continue;
-				}
+					}
 				//Todo ADD >=, <=
 				else if(inst == IF) {
 					if(textOperand[0] == '>') {
@@ -561,11 +962,11 @@ static inline u64 textToProgram(const char* name, Instruction *instrucion) {
 
 static inline void programToBin(const char* name, Instruction *instruction, u64 numOfInstruction) {
 
-	FILE *f = fopen(name, "wb");
+	FILE *f = fopen(name, "wb+");
 	if(f == NULL) {
 		ERROR_BREAK("FILE ERROR !!!\n");
 		}
-	fwrite(instruction, sizeof(Instruction), numOfInstruction, f);
+	fwrite(instruction, sizeof(Instruction), numOfInstruction+1, f);
 	fclose(f);
 
 
@@ -582,5 +983,6 @@ static inline void binToProgram(const char* name, Instruction *instruction) {
 	fclose(f);
 	}
 
+#endif
 
 #endif
